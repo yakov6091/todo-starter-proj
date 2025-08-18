0da5e6dc4ccd9610ed6bf87970f8a4c0156c9ba1
Yakov <yakov6091@gmail.com>
2025-08-18 14:08:29 +0300

    Add actions folder and todo.actions.js

---
+++ store/actions/todo.actions.js
@@ -0,0 +1,88 @@
+import { todoService } from '../../services/todo.service.js'
+import {
+    SET_IS_LOADING,
+    ADD_TODO,
+    REMOVE_TODO,
+    SET_TODOS,
+    UPDATE_TODO,
+    store,
+    SET_DONE_TODOS_PERCENT,
+    SET_MAX_PAGE,
+    SET_FILTER_BY,
+} from '../store.js'
+import { addActivity } from './user.actions.js'
+
+export function loadTodos(filterSort) {
+    store.dispatch({ type: SET_IS_LOADING, isLoading: true })
+
+    return todoService.query(filterSort)
+        .then(({ todos, maxPage, doneTodos }) => {
+            store.dispatch({
+                type: SET_TODOS,
+                todos
+            })
+            _setTodosData(doneTodos, maxPage)
+            return todos
+        })
+        .catch((err) => {
+            console.error('Cannot load todos:', err)
+            throw err
+        })
+        .finally(() => {
+            store.dispatch({ type: SET_IS_LOADING, isLoading: false })
+        })
+
+}
+
+export function saveTodo(todo) {
+    const type = todo._id ? UPDATE_TODO : ADD_TODO
+    return todoService.save(todo)
+        .then(({ maxPage, doneTodos, savedTodo }) => {
+            store.dispatch({
+                type,
+                todo: savedTodo,
+            })
+            _setTodosData(doneTodos, maxPage)
+            return savedTodo
+        })
+        .then((res) => {
+            const actionName = todo._id ? 'Updated' : 'Added'
+            return addActivity(`${actionName} a Todo: ` + todo.txt).then(() => res)
+        })
+        .catch((err) => {
+            console.error('Cannot save todo:', err)
+            throw err
+        })
+}
+
+export function removeTodo(todoId) {
+    return todoService
+        .remove(todoId)
+        .then(({ maxPage, doneTodos }) => {
+            store.dispatch({
+                type: REMOVE_TODO,
+                todoId,
+            })
+            _setTodosData(doneTodos, maxPage)
+        })
+        .then(() => addActivity('Removed the Todo: ' + todoId))
+        .catch((err) => {
+            console.error('Cannot remove todo:', err)
+            throw err
+        })
+}
+
+
+function _setTodosData(doneTodosPercent, maxPage) {
+    store.dispatch({
+        type: SET_DONE_TODOS_PERCENT,
+        doneTodosPercent,
+    })
+    store.dispatch({
+        type: SET_MAX_PAGE,
+        maxPage,
+    })
+
+
+}
+
